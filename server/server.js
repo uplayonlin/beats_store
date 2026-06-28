e2558022d192c8eb5b61cccb4ad019e9b57832e5
ILYA-PC <uplayonlin@gmail.com>
2026-06-28 22:39:31 +0300

    Fix server to listen on 0.0.0.0 for Docker

--- server/server.js
+++ server/server.js
@@ -1228,8 +1228,6 @@ app.use((req, res) => {
 //     console.log(`🎹 samplekitsStore: http://localhost:${PORT}/samplekits.html`);
 // });
 
-const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
     console.log(`📁 Uploads: ${uploadsDir}`);
